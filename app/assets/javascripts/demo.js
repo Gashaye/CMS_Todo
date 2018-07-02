06e0f440a076157404d55429409d1d246bc48344
Goshu Anley <gashayem@gmail.com>
2018-07-01 21:16:02 -0400

    This is the second Nav commit

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -11,3 +11,11 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
